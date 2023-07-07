38e48d9480b4fba7c292a0369691df4d695776d2
Goodnessmbakara <amicablembakara50@gmail.com>
2023-07-07 22:42:29 +0100

    adding floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Module that returns float sum of two floats"""
+
+def add(a:float, b:float) -> float:
+    """Function add sums to floats
+  
+    Args:
+    a (float): First float number
+    b (float): Second float number
+
+    Returns:
+      float: Sum of the two float numbers
+    """
+    return a + b
+  
