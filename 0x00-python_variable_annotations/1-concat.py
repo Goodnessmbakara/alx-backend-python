348267d8a58387a647ef17268c6d2ae25cefcacf
Goodnessmbakara <amicablembakara50@gmail.com>
2023-07-07 23:13:43 +0100

    some babies

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
-
-"""Module for concatenating two strings."""
-
-def concat(str1:str,str2:str)->str:
+def concat(str1: str, str2: str) -> str:
     """
-    Function concat: Concatenates Strings.
-    
-    Args: 
-    str1 (string): First string.
-    str2 (string): Second  string.
-    
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
     """
     return str1 + str2
-  
