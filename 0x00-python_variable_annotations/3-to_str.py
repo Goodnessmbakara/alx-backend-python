eedb88b9ea474dfa6b6e6614d9fe45aba226d628
Goodnessmbakara <amicablembakara50@gmail.com>
2023-07-11 09:23:27 +0100

    floats could be strings

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module Three to_str.
+"""
+
+
+def to_str(n_float: float) -> str:
+    """to_str Function computates the string representation of a float"""
+    return str(n_float)
