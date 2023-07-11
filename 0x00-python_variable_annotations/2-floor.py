65441f2ec5c72871b1660c8184eacbbdaf115be8
Goodnessmbakara <amicablembakara50@gmail.com>
2023-07-11 09:12:48 +0100

    floor is slippery

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Module emulates maths.floor"""
+
+def floor(n_float: float)->int:
+    """Floor function computes the floor of a float
+    Parameters: n(float)
+    Returns: Float
+    """
+    return int(n_float)
